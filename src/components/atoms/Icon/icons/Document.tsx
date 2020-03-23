58f9aad6c8b699f990dc2b8b131344a527edaaae
Patryk Staniewski <exihuatl@gmail.com>
2020-03-23 16:03:35 +0100

    Update "Document" icon

--- src/components/atoms/Icon/icons/Document.tsx
+++ src/components/atoms/Icon/icons/Document.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export const Document = {
-  viewBox: '0 0 9 12',
-  jsx: <path fill="currentColor" d="M0 0v12h9V3.2L5.727 0z" />,
+  viewBox: '0 0 41 55',
+  jsx: (
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M26.0909091,0 L41,14.6666667 L41,21 L20,21 L20,0 L26.0909091,0 Z M16,23 L16.0054857,23.1492623 C16.0818349,24.1841222 16.9456382,25 18,25 L18,25 L41,25 L41,55 L0,55 L0,0 L16,0 L16,23 Z"
+    />
+  ),
 };
