f08437e7fb5862d9dca47b3a2f574a2ece52653e
Patryk Staniewski <exihuatl@gmail.com>
2020-07-16 16:14:38 +0300

    Add `menuProps` to tooltip

--- src/components/atoms/Tooltip/Tooltip.tsx
+++ src/components/atoms/Tooltip/Tooltip.tsx
@@ -1,54 +1,56 @@
-// @TODO: Nessesary draft for "Tie" component. API will probably change, so it's not exported from core.
-
 import React, { FC, memo } from 'react';
 import { Box, BoxProps } from '../../quarks';
-import { Menu } from '../Menu';
+import { Menu, MenuProps } from '../Menu';
 
-export const Tooltip: FC<ITooltipProps> = memo(({ layer, sx, onMouseLeave, ...props }) => {
-  const mouseLeave = (callback: () => void) => (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    callback();
-    return setTimeout(() => onMouseLeave?.(event), 250);
-  };
+export const Tooltip: FC<ITooltipProps> = memo(
+  ({ layer, sx, menuProps, onMouseLeave, ...props }) => {
+    const mouseLeave = (callback: () => void) => (
+      event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+      callback();
+      return setTimeout(() => onMouseLeave?.(event), 250);
+    };
 
-  if (!layer) {
-    return <Box sx={sx} {...props} />;
+    if (!layer) {
+      return <Box sx={sx} {...props} />;
+    }
+
+    return (
+      <Menu
+        fixed={false}
+        {...menuProps}
+        trigger={p => (
+          <Box
+            sx={{ position: 'relative', ...sx }}
+            onMouseEnter={p.open}
+            onMouseLeave={mouseLeave(p.close)}
+            ref={p.triggerRef}
+            {...props}
+          />
+        )}
+        layer={({ sx: layerSX, ...props }) => (
+          <Box
+            sx={{
+              ...layerSX,
+              minWidth: 'max-content',
+              maxWidth: '300px',
+              border: 'none',
+              fontScale: 2,
+              p: 2,
+              bg: '1',
+              color: 'white',
+            }}
+            {...props}
+          >
+            {layer}
+          </Box>
+        )}
+      />
+    );
   }
-
-  return (
-    <Menu
-      fixed={false}
-      trigger={p => (
-        <Box
-          sx={{ position: 'relative', ...sx }}
-          onMouseEnter={p.open}
-          onMouseLeave={mouseLeave(p.close)}
-          ref={p.triggerRef}
-          {...props}
-        />
-      )}
-      layer={({ sx: layerSX, ...props }) => (
-        <Box
-          sx={{
-            ...layerSX,
-            minWidth: 'max-content',
-            maxWidth: '300px',
-            border: 'none',
-            fontScale: 2,
-            p: 2,
-            bg: '1',
-            color: 'white',
-          }}
-          {...props}
-        >
-          {layer}
-        </Box>
-      )}
-    />
-  );
-});
+);
 
 interface ITooltipProps extends BoxProps {
   layer?: React.ReactChild;
+  menuProps?: Omit<MenuProps, 'trigger' | 'layer'>;
 }
