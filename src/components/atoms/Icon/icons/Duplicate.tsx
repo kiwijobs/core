9d6c8d446cb61d583cbed316b88641f24fd6c2c3
Patryk Staniewski <exihuatl@gmail.com>
2020-07-17 09:28:40 +0300

    Add new icons

---
+++ src/components/atoms/Icon/icons/Duplicate.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Duplicate = {
+  viewBox: '0 0 24 24',
+  jsx: (
+    <g fill="currentColor" fillRule="nonzero">
+      <path d="M15.375 6H6v9.375h1.875v-7.5h7.5z" />
+      <path d="M8.625 8.625V18H18V8.625H8.625zm7.125 5.063h-2.063v2.062a.375.375 0 0 1-.75 0v-2.063h-2.062a.375.375 0 0 1 0-.75h2.063v-2.062a.375.375 0 0 1 .75 0v2.063h2.062a.375.375 0 0 1 0 .75z" />
+    </g>
+  ),
+};
---
+++ src/components/atoms/Icon/icons/Pen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Pen = {
+  viewBox: '0 0 24 24',
+  jsx: (
+    <path
+      fill="currentColor"
+      d="M5.508 15.28l-1.026 4.046 3.785-1.21zM15.733 5.566L17.3 4 20 6.7l-1.566 1.567zM5.991 14.868l8.877-8.877 2.856 2.857-8.876 8.876z"
+    />
+  ),
+};
